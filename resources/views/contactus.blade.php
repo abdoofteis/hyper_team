27290aaace876e14595b0f58c20c2f0259fe5744
AbdooFtais <abdoofteis1@gmail.com>
2019-12-11 04:58:29 +0200

    first commit

---
+++ resources/views/contactus.blade.php
@@ -0,0 +1,151 @@
+<html>
+    <head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{ asset('css/contact-us.css') }}">
+
+    </head>
+    <body style ="background-image: url('{{ asset('img/bg.jpg') }}');">
+          <!-- header start -->
+    
+  <header>
+        <div class="header-area "> 
+            <div id="sticky-header" class="main-header-area">
+                <div class="container">
+                    <div class="header_bottom_border">
+                        <div class="row align-items-center">
+                            <div class="col-xl-3 col-lg-2">
+                                <div class="logo">
+                                    <a href="http://localhost/Hyper-team1/public/hyperteam">
+                                        <img src="{{asset('img/logo2.png')}}" alt="Hypeer Team">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-xl-6 col-lg-7">
+                                <div class="main-menu  d-none d-lg-block">
+                                    <nav>
+                                        <ul id="navigation">
+                                            <li><a class="active" href="http://localhost/Hyper-team1/public/hyperteam">Home</a></li>
+                                            <li><a href="#">Protfolio <i class="ti-angle-down"></i></a></li>
+                                            <li><a href="#">Services</a></li>
+                                            <li><a href="#">About Us <i class="ti-angle-down"></i></a>
+                                            </li>
+                                            <li><a href="http://localhost/Hyper-team1/public/hyperteam/contactus">Contact</a></li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-3 d-none d-lg-block">
+                                <div class="Appointment">
+                                    <div class="book_btn d-none d-lg-block">
+                                        <a  href="http://localhost/Hyper-team1/public/hyperteam/contactus">CONTACT US</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="mobile_menu d-block d-lg-none pt-2"></div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- header-end -->
+
+
+
+    <div class="wrapper">
+        <div class="content">
+                <h2>CONTACT US</h2>
+                <p>How We Can Help You Sir...</p>
+        </div>
+        <div class="container mb-5 ">
+               
+            <form action="">
+                
+              <div class="row form-group">
+              <label class="col-lg-2 col-md-3 col-sm-12 text-md-right text-sm-center">Your Name</label>
+              <input class="col-lg-7 col-md-9 col-sm-12 form-control "type="text" placeholder="Ahmed">
+              </div>
+              
+               <div class="row form-group">
+              <label class="col-lg-2 col-md-3 col-sm-12 text-md-right text-sm-center">E-Mail</label>
+              <input class="col-lg-7 col-md-9 col-sm-12 form-control "type="text" placeholder="Example@example.com">
+              </div>
+              
+               <div class="row form-group">
+              <label class="col-lg-2 col-md-3 col-sm-12 text-md-right text-sm-center">Phone Number</label>
+              <input class="col-lg-7 col-md-9 col-sm-12 form-control "type="text" placeholder="0912345678">
+              </div>
+              
+             <div class="row">
+            <label class="col-lg-2 col-md-3 col-sm-12 text-md-right text-sm-center ">Choose</label>
+            <select class="col-lg-7 col-md-9 col-sm-12 form-control  custom-select " >
+                <option>WebSite</option>
+                <option>App</option>
+                <option>Website & App</option>
+                <option>Desgin</option>
+                <option>Other</option>
+            </select>
+              </div>
+              
+              <div class="row form-group mt-3 mb-3">
+          <label class= "col-lg-2 col-md-3 col-sm-12 text-md-right text-sm-center">Message</label>
+          <textarea class="col-lg-7 col-md-9 col-sm-12 form-control" rows="3"placeholder="Your Message" ></textarea>
+        </div>
+              
+                <div class="btn">Send</div>
+            </div>
+              
+            </form>
+          </div>
+        </div>
+ <!-- Footer Start -->
+ <div class="footer-main-div">
+    <div class="footer-social-icons">
+        <ul>
+           <li><a href="#" target="blank"><i class="fa fa-facebook"></i></a></li>
+           <li><a href="#" target="blank"><i class="fa fa-twitter"></i></a></li>
+           <li><a href="#" target="blank"><i class="fa fa-google-plus"></i></a></li>
+           <li><a href="#" target="blank"><i class="fa fa-youtube"></i></a></li>
+        </ul>
+    </div>
+
+    <div class="footer-menu-one">
+       <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">Protfolio</a></li>
+          <li><a href="">Our Team</a></li>
+       </ul>
+    </div>
+ 
+    <div class="footer-menu-two">
+       <ul>
+          <li><a href="">Services</a></li>
+          <li><a href="http://localhost/Hyper-team1/public/hyperteam/contactus">Contact Us</a></li>
+       </ul>
+    </div>
+
+</div>
+<div class="footer-bottom">
+<p>Designed by:<a href="http://localhost/Hyper-team1/public/hyperteam">Hyper Team</a></p>
+</div>
+<!-- Footer End -->
+
+
+
+<script type="text/javascript" src="{{asset('js/team.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/jquery-3.4.1.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/jquery.cycle2.min.js')}}"></script>
+
+<script src="{{asset('js/vendor/jquery-1.12.4.min.js')}}"></script>
+<script src="{{asset('js/popper.min.js')}}"></script>    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+<script src="{{asset('js/owl.carousel.min.js')}}"></script>
+<script src="{{asset('js/jquery.slicknav.min.js')}}"></script>
+<script src="{{asset('js/main.js')}}"></script>
+</body>
+</html>
---
+++ resources/views/hyperteam.blade.php
@@ -0,0 +1,302 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+   <title>Hyper-Team</title>
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+   <link rel="stylesheet" href="{{ asset('css/team.css') }}">
+   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/slicknav.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/nav.css') }}">
+</head>
+<body>
+  <!-- header start -->
+    
+  <header>
+        <div class="header-area "> 
+            <div id="sticky-header" class="main-header-area">
+                <div class="container">
+                    <div class="header_bottom_border">
+                        <div class="row align-items-center">
+                            <div class="col-xl-3 col-lg-2">
+                                <div class="logo">
+                                    <a href="http://localhost/Hyper-team1/public/hyperteam#">
+                                        <img src="img/logo2.png" alt="Hypeer Team">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-xl-6 col-lg-7">
+                                <div class="main-menu  d-none d-lg-block">
+                                    <nav>
+                                        <ul id="navigation">
+                                            <li><a class="active" href="http://localhost/Hyper-team1/public/hyperteam#">Home</a></li>
+                                            <li><a href="#">Protfolio <i class="ti-angle-down"></i></a></li>
+                                            <li><a href="#">Services</a></li>
+                                            <li><a href="#">About Us <i class="ti-angle-down"></i></a>
+                                            </li>
+                                            <li><a href="http://localhost/Hyper-team1/public/hyperteam/contactus">Contact</a></li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-3 d-none d-lg-block">
+                                <div class="Appointment">
+                                    <div class="book_btn d-none d-lg-block">
+                                        <a  href="http://localhost/Hyper-team1/public/hyperteam/contactus">CONTACT US</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="mobile_menu d-block d-lg-none pt-2"></div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- header-end -->
+
+    <!-- slider_area_start -->
+    <div class="slider_area">
+        <div class="slider_active owl-carousel">
+            <div class="single_slider  d-flex align-items-center slider_bg_1 overlay2"><h1 style="font-size: 130px;
+color: #e9b422;
+margin-left: 600px; margin-top: -70px;">Hello</h1><h1 class="text-slide2">Welcome To Our WebSite...</h1></p></div>
+
+            <div class="single_slider  d-flex align-items-center slider_bg_2 overlay2"><h1 style="font-size: 100px;
+color: #e9b422;
+margin-left: 580px; margin-top: -80px;">We Are <h1 class="text-slide2">Web Developers...</h1></h1></div>
+
+            <div class="single_slider  d-flex align-items-center slider_bg_3 overlay2"><h1 style="font-size: 150px;
+color: #e9b422;
+margin-left: 750px; margin-top: -80px;">& <h1 style="color: white;
+font-size: 50px;margin-top: 150px;margin-left: -200px;">Web Designers...</h1></h1></div>
+
+            <div class="single_slider  d-flex align-items-center slider_bg_4 overlay2"><h1 style="font-size: 80px;
+color: #e9b422;
+margin-left: 400px; margin-top: -70px;">We Are Always Ready <h1 class="text-slide2">To Serve You...</h1></h1></div>
+
+           
+        </div>
+    </div>
+    <!-- slider_area_end -->
+
+   <!-- header end -->
+
+    <!-- text slide start-->
+     <div class="wrapper">
+        <h2><b>We Are </b><span class="change"></span></h2>
+     </div>
+     <br></br>
+   <!-- text slide end -->
+
+      <!-- protfolio div start -->
+       <h1 class="prot"><b>PROTFOLIO</b></h1>
+      <!-- slide start -->
+   <div class="cycle-slideshow pb-2"
+    data-cycle-speed="600"
+      data-cycle-manual-speed="800"
+      data-cycle-fx='scrollHorz'>
+    <!-- start controller -->
+        <span class="cycle-prev"style="left: 3%"><b>&#60;</b></span>
+        <span class="cycle-next"style="right: 3%"><b>&#62;</b></span>
+        <span class="cycle-pager"></span>
+    <!-- end controller -->
+        <img class="b" src="{{asset('img/couch-conference-concentration-startup-7111.jpg')}}">
+        <img class="b" src="{{asset('img/adults-business-computer-desk-1181371.jpg')}}">
+        <img class="b" src="{{asset('img/woman-wearing-black-and-red-gingham-sport-shirt-1181471.jpg')}}">
+   </div>
+<!-- slide end -->
+<!-- end protfolio -->
+<!-- services start -->
+     <h1 class="ser" style="color:#e9b422;"><b>OUR SERVICES</b></h1>
+      <div class="container">
+         <div class="row">
+            <div class="col-md-3 col-sm-6">
+               <div class="service-box">
+                  <i class="fa fa-globe"></i>
+                  <h4 class="title">Web Design</h4>
+                  <p class="description">
+                     Hello Hyper Team With You We Can Help You For Any Design You Want Just Contact With Us</p>
+               </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+               <div class="service-box">
+                  <i class="fa fa-rocket"></i>
+                  <h4 class="title">Web Development</h4>
+                  <p class="description">
+                     Hello Hyper Team With You We Can Help You For Any Design You Want Just Contact With Us</p>
+               </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+               <div class="service-box">
+                  <i class="fa fa-edit"></i>
+                  <h4 class="title">SEO</h4>
+                  <p class="description">
+                     Hello Hyper Team With You We Can Help You For Any Design You Want Just Contact With Us</p>
+               </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+               <div class="service-box">
+                  <i class="fa fa-cog"></i>
+                  <h4 class="title">Social Networks</h4>
+                  <p class="description">
+                     Hello Hyper Team With You We Can Help You For Any Design You Want Just Contact With Us</p>
+               </div>
+            </div>
+       </div>
+    </div>
+<!-- services End -->
+<!-- Our Team start -->
+<br></br>
+  <section>
+         <h1 class="prot"><b>OUR TEAM</b></h1>
+           <div class="card-wrapper">
+            <div class="card">
+               <img src="img/small2.jpg" alt="Malek Alnapouli" class="card-img">
+               <img src="img/malek1.jpg" alt="profile img" class="profile-img">
+               <h1>Malek AL Napouli</h1>
+               <p class="job-title">Lead Designer</p>
+               <p class="about">Hello people I'm Malek Study at Tripoli University Fuctily of sience I Work as web Designer & I'm good of it </p>
+               <ul class="social-media">
+                <li><a href="https://www.facebook.com/Maleknp97"><i class="fab fa-facebook-square"></i></a></li>
+                <li><a href="#"><i class="fab fa-twitter-square"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                <li><a href="#"><i class="fab fa-google-plus-square"></i></a></li>
+               </ul>
+            </div>
+            <div class="card">
+               <img src="img/small2.jpg" alt="Malek Alnapouli" class="card-img">
+               <img src="img/Abdoo.jpg" alt="profile img" class="profile-img">
+               <h1>Abdo Fteis</h1>
+               <p class="job-title">Full Stack Developer</p>
+               <p class="about">Hello people I'm Abdo Study at Tripoli University Fuctily of sience I Work as web Designer & I'm good of it </p>
+               <ul class="social-media">
+                <li><a href="https://www.facebook.com/profile.php?id=100003436528749"><i class="fab fa-facebook-square"></i></a></li>
+                <li><a href="#"><i class="fab fa-twitter-square"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                <li><a href="#"><i class="fab fa-google-plus-square"></i></a></li>
+               </ul>
+            </div>
+            <div class="card">
+               <img src="img/small2.jpg" alt="Malek Alnapouli" class="card-img">
+               <img src="img/hussain.jpg" alt="profile img" class="profile-img">
+               <h1>Hussain Moftah</h1>
+               <p class="job-title">Web Developer</p>
+               <p class="about">Hello people I'm Hussain Study at Tripoli University Fuctily of sience I Work as web Designer & I'm good of it </p>
+               <ul class="social-media">
+                <li><a href="https://www.facebook.com/hussain.muftah"><i class="fab fa-facebook-square"></i></a></li>
+                <li><a href="#"><i class="fab fa-twitter-square"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                <li><a href="#"><i class="fab fa-google-plus-square"></i></a></li>
+               </ul>
+            </div>
+            <div class="card">
+               <img src="img/small2.jpg" alt="Malek Alnapouli" class="card-img">
+               <img src="img/ibrahim.jpg" alt="profile img" class="profile-img">
+               <h1>Ibrahim Shwekh</h1>
+               <p class="job-title">Full Stack Developer</p>
+               <p class="about">Hello people I'm Ibrahim Study at Tripoli University Fuctily of sience I Work as web Designer & I'm good of it </p>
+               <ul class="social-media">
+                <li><a href="https://www.facebook.com/www.ibrahim.shwekh"><i class="fab fa-facebook-square"></i></a></li>
+                <li><a href="#"><i class="fab fa-twitter-square"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                <li><a href="#"><i class="fab fa-google-plus-square"></i></a></li>
+               </ul>
+            </div>
+    </div>
+  </section>
+<!-- Our Team End -->
+<!-- About Us Start -->
+      <div class="about-section">
+         <div class="inner-width">
+            <h1><b>ABOUT US</b></h1>
+            <div class="border"></div>
+            <div class="about-section-row">
+               <div class="about-section-col">
+                  <div class="about">
+                     <p>our team member student at university of tripoli we study together & word together our team is the best at web.
+                     </p>
+                     <a href="#">CONTACT US</a>
+                  </div>
+               </div>
+               <div class="about-section-col">
+                  <div class="skills">
+                     <div class="skills">
+                        <div class="title">Web Design</div>
+                        <div class="progress">
+                           <div class="progress-bar p1"><span>90%</span></div>
+                        </div>
+                     </div>
+
+                     <div class="skills">
+                        <div class="title">UI Design</div>
+                        <div class="progress">
+                           <div class="progress-bar p2"><span>70%</span></div>
+                        </div>
+                     </div>
+
+                     <div class="skills">
+                        <div class="title">UX Design</div>
+                        <div class="progress">
+                           <div class="progress-bar p3"><span>50%</span></div>
+                        </div>
+                     </div>
+
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+<!-- About Us End -->
+ <!-- Footer Start -->
+    <div class="footer-main-div">
+           <div class="footer-social-icons">
+               <ul>
+                  <li><a href="#" target="blank"><i class="fa fa-facebook"></i></a></li>
+                  <li><a href="#" target="blank"><i class="fa fa-twitter"></i></a></li>
+                  <li><a href="#" target="blank"><i class="fa fa-google-plus"></i></a></li>
+                  <li><a href="#" target="blank"><i class="fa fa-youtube"></i></a></li>
+               </ul>
+           </div>
+
+           <div class="footer-menu-one">
+              <ul>
+                 <li><a href="">Home</a></li>
+                 <li><a href="">Protfolio</a></li>
+                 <li><a href="">Our Team</a></li>
+              </ul>
+           </div>
+        
+           <div class="footer-menu-two">
+              <ul>
+                 <li><a href="">Services</a></li>
+                 <li><a href="http://localhost/Hyper-team1/public/hyperteam/contactus">Contact Us</a></li>
+              </ul>
+           </div>
+
+    </div>
+    <div class="footer-bottom">
+       <p>Designed by:<a href="http://localhost/Hyper-team1/public/hyperteam#">Hyper Team</a></p>
+    </div>
+ <!-- Footer End -->
+ <!-- {{ asset('js/jqurey*3.4.1.min.js') }} -->
+ <script type="text/javascript" src="{{asset('js/team.js')}}"></script>
+ <script type="text/javascript" src="{{asset('js/jquery-3.4.1.min.js') }}"></script>
+ <script type="text/javascript" src="{{asset('js/jquery.cycle2.min.js')}}"></script>
+
+ <script src="{{asset('js/vendor/jquery-1.12.4.min.js')}}"></script>
+ <script src="{{asset('js/popper.min.js')}}"></script>    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+ <script src="{{asset('js/owl.carousel.min.js')}}"></script>
+ <script src="{{asset('js/jquery.slicknav.min.js')}}"></script>
+ <script src="{{asset('js/main.js')}}"></script>
+</body>
+</html>
